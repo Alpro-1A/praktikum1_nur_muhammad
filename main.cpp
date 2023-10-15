c3bff16d906115c4f285c930a3d85708a2125857
mamad <2310631170145@student.unsika.ac.id>
2023-10-15 15:01:45 +0700

    menambahkan file main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main()
+{
+	std::cout <<"-----------------------------------------------------\n\n";
+	std::cout<<"Nama: Nur Muhammad\n\n";
+	std::cout<<"NPM: 2310631170145\n\n";
+	std::cout<<"Kelas: A - Informatika\n\n";
+	std::cout<<"Cita - Cita: Software Engineer\n\n";
+	std::cout<<"Motivasi Masuk Informatika: Prospek Kerja yang Luas\n\n";
+	std::cout <<"-----------------------------------------------------\n\n";
+	return 0;
+}
